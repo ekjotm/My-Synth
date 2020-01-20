126cd62e18f66c5f780fc22e2042bf94546d769c
ekjotm <43255916+ekjotm@users.noreply.github.com>
2020-01-20 12:42:43 -0500

    Add files via upload

--- My Synth/My Synth/ViewController.swift
+++ My Synth/My Synth/ViewController.swift
@@ -225,5 +225,166 @@ class ViewController: UIViewController {
         noteStop(freq: 493.88)
     }
     
+    @IBAction func C2Button(_ sender: UIButton) {
+        print("C2")
+        sender.backgroundColor = UIColor.init(white: 0.1, alpha: 0.2)
+              
+        noteStart(freq: 523.25)
+    }
+    
+    @IBAction func C2ButtonUp(_ sender: UIButton) {
+        print("C2 up")
+        sender.backgroundColor = UIColor.init(white: 0, alpha:0)
+                 
+        noteStop(freq: 523.25)
+    }
+    
+    @IBAction func D2Button(_ sender: UIButton) {
+        print("D2")
+               sender.backgroundColor = UIColor.init(white: 0.1, alpha: 0.2)
+                     
+               noteStart(freq: 587.33)
+        
+    }
+    
+    @IBAction func D2ButtonUp(_ sender: UIButton) {
+        print("D2 up")
+        sender.backgroundColor = UIColor.init(white: 0, alpha:0)
+                 
+        noteStop(freq: 587.33)
+    }
+    
+    @IBAction func E2Button(_ sender: UIButton) {
+        print("E2")
+        sender.backgroundColor = UIColor.init(white: 0.1, alpha: 0.2)
+              
+        noteStart(freq: 659.25)
+    }
+    
+    @IBAction func E2ButtonUp(_ sender: UIButton) {
+        print("E2 up")
+        sender.backgroundColor = UIColor.init(white: 0, alpha:0)
+                 
+        noteStop(freq: 659.25)
+    }
+    
+    @IBAction func F2Button(_ sender: UIButton) {
+        print("F2")
+               sender.backgroundColor = UIColor.init(white: 0.1, alpha: 0.2)
+                     
+               noteStart(freq: 698.46)
+    }
+    
+    @IBAction func F2ButtonUp(_ sender: UIButton) {
+        print("F2 up")
+               sender.backgroundColor = UIColor.init(white: 0, alpha:0)
+                        
+               noteStop(freq: 698.46)
+    }
+    
+    @IBAction func G2Button(_ sender: UIButton) {
+        print("G2")
+        sender.backgroundColor = UIColor.init(white: 0.1, alpha:0.2)
+                 
+        noteStart(freq: 783.99)
+    }
+    
+    @IBAction func G2ButtonUp(_ sender: UIButton) {
+        print("G2 up")
+        sender.backgroundColor = UIColor.init(white: 0, alpha:0)
+                 
+        noteStop(freq: 783.99)
+    }
+    
+    @IBAction func A2Button(_ sender: UIButton) {
+        print("A2")
+        sender.backgroundColor = UIColor.init(white: 0.1, alpha:0.2)
+                 
+        noteStart(freq: 880.00)
+    }
+    
+    @IBAction func A2ButtonUp(_ sender: UIButton) {
+        print("A2 up")
+             sender.backgroundColor = UIColor.init(white: 0, alpha:0)
+                      
+             noteStop(freq: 880.00)
+    }
+    
+    @IBAction func B2Button(_ sender: UIButton) {
+        print("B2")
+        sender.backgroundColor = UIColor.init(white: 0.1, alpha:0.2)
+                 
+        noteStart(freq: 987.77)
+    }
+    
+    @IBAction func B2ButtonUp(_ sender: UIButton) {
+        print("B2 up")
+             sender.backgroundColor = UIColor.init(white: 0, alpha:0)
+                      
+             noteStop(freq: 987.77)
+    }
+    
+    @IBAction func c2Button(_ sender: UIButton) {
+        print("c2")
+        
+        noteStart(freq: 554.37)
+    }
+    
+    @IBAction func c2ButtonUp(_ sender: UIButton) {
+        print("c2 up")
+        
+        noteStop(freq: 554.37)
+    }
+    
+    @IBAction func d2Button(_ sender: UIButton) {
+        print("d2")
+        
+        noteStart(freq: 622.25)
+    }
+    
+    @IBAction func d2ButtonUp(_ sender: UIButton) {
+        print("d2 up")
+        
+        noteStop(freq: 622.25)
+    }
+    
+    @IBAction func f2Button(_ sender: UIButton) {
+        print("f2")
+        
+        noteStart(freq: 739.99)
+        
+    }
+    
+    @IBAction func f2ButtonUp(_ sender: UIButton) {
+        print("f2 up")
+        
+        noteStop(freq: 739.99)
+    }
+    
+    @IBAction func g2Button(_ sender: UIButton) {
+        print("g2")
+        
+        noteStart(freq: 830.61)
+    }
+    
+    @IBAction func g2ButtonUp(_ sender: UIButton) {
+        print("g2 up")
+        
+        noteStop(freq: 830.61)
+    }
+    
+    @IBAction func a2Button(_ sender: UIButton) {
+        print("a2")
+        
+        noteStart(freq: 932.33)
+    }
+    
+    @IBAction func a2ButtonUp(_ sender: UIButton) {
+        print("a2 up")
+        
+        noteStop(freq: 932.33)
+    }
+    
+    
 }
 
